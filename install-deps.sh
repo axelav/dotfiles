c79848d2617caf8e6629843db9b6e5a179240bea
Axel Anderson <axel@axelav.com>
2014-07-31 21:22:16 -0400

    install global node modules

--- install-deps.sh
+++ install-deps.sh
@@ -9,18 +9,8 @@ ruby <(curl -fsSkL raw.github.com/mxcl/homebrew/go)
 # install homebrew packages
 ./.brew
 
-# install npm globals
-npm i -g browserify
-npm i -g coffee-script-redux
-npm i -g express
-npm i -g grunt-cli
-npm i -g gulp
-npm i -g jshint
-npm i -g lute
-npm i -g rainbowify
-npm i -g spotify-cli
-npm i -g trash
-npm i -g forecast
+# install global node modules
+./.node-modules
 
 # https://github.com/rupa/z
 mkdir -p ~/code/z
