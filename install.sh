86d98e619b3f3173e2bfe5819f75846517773aae
Axel Anderson <axel@ftml.net>
2021-12-01 19:53:43 -0500

    feat: use fish shell

--- install.sh
+++ install.sh
@@ -37,6 +37,10 @@ stow \
   zsh \
   --verbose
 
+# install fundle
+# https://github.com/danhper/fundle
+curl -sfL https://git.io/fundle-install | fish
+
 # install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
