538f111f99c96037911cb8ef40970854be8243ef
Axel Anderson <axel@ftml.net>
2023-01-13 07:02:12 -0500

    Use startify dashboard for lunarvim startup

--- lunarvim/.config/lvim/config.lua
+++ lunarvim/.config/lvim/config.lua
@@ -67,7 +67,7 @@ lvim.keys.normal_mode["<leader>a"] = ":Telescope grep_string<cr>"
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
 lvim.builtin.alpha.active = true
-lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.mode = "startify"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.git.enable = true
 lvim.builtin.nvimtree.setup.view.adaptive_size = true
