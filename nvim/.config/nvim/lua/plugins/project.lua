e5a1369d5e80badd359f5a2e6a40717405d98378
Axel Anderson <axel@ftml.net>
2023-08-14 18:52:22 -0400

    Add project.nvim config

---
+++ nvim/.config/nvim/lua/plugins/project.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "ahmedkhalf/project.nvim",
+    opts = {
+      -- Don't calculate root dir on specific directories
+      -- Ex: { "~/.cargo/*", ... }
+      exclude_dirs = {
+        "~/s/super/firebase-functions/*",
+      },
+      show_hidden = true,
+      silent_chdir = false,
+    },
+    event = "VeryLazy",
+    config = function(_, opts)
+      require("project_nvim").setup(opts)
+      require("telescope").load_extension("projects")
+    end,
+    keys = {
+      { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+    },
+  },
+}
