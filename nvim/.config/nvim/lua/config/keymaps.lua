26c459b0405de7928411483536b9a6e3a07225fa
Axel Anderson <axel@ftml.net>
2023-08-15 07:51:09 -0400

    Disable custom telescope keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,13 +2,13 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.set("n", "<C-p>", function()
-  require("telescope.builtin").find_files()
-end, { desc = "Find files" })
-
-vim.keymap.set("n", "<C-f>", function()
-  require("telescope.builtin").live_grep()
-end, { desc = "Grep files" })
+-- vim.keymap.set("n", "<C-p>", function()
+--   require("telescope.builtin").find_files()
+-- end, { desc = "Find files" })
+--
+-- vim.keymap.set("n", "<C-f>", function()
+--   require("telescope.builtin").live_grep()
+-- end, { desc = "Grep files" })
 
 vim.keymap.set("n", "<leader><space>", function()
   require("telescope.builtin").git_files()
