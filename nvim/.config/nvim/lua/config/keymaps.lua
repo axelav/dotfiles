149ee8c20379465414374de4eed6ad9d311f550b
Axel Anderson <axel@ftml.net>
2023-09-25 17:21:20 -0400

    Clean up neovim keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -2,21 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- vim.keymap.set("n", "<C-p>", function()
---   require("telescope.builtin").find_files()
--- end, { desc = "Find files" })
---
--- vim.keymap.set("n", "<C-f>", function()
---   require("telescope.builtin").live_grep()
--- end, { desc = "Grep files" })
-
 vim.keymap.set("n", "<leader><space>", function()
   require("telescope.builtin").git_files()
 end, { desc = "Git files" })
+
+-- vim.keymap.del("t", "<C-->")
+-- vim.keymap.del("t", "<C-/>")
 
 vim.keymap.set(
   "n",
   "<leader>gb",
   '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
-  { silent = true }
+  {
+    silent = true,
+    desc = "Open link to current line on GitHub",
+  }
 )
