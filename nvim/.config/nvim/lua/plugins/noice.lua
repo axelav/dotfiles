5a3226cb57dcc85cde33091791bc1046e377a81e
Axel Anderson <axel@ftml.net>
2023-11-30 19:45:33 -0500

    Display borders around nvim hover/dialog windows

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,6 +1,17 @@
+-- return {
+--   {
+--     "folke/noice.nvim",
+--     enabled = false,
+--   },
+-- }
+
 return {
   {
     "folke/noice.nvim",
-    enabled = false,
+    opts = {
+      presets = {
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
   },
 }
