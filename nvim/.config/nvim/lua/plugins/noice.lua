1f0213551644786b21d9c7026e01a7a7fd557722
Axel Anderson <axel@ftml.net>
2024-10-08 08:48:58 -0400

    Filter nvim `noice` inlayHint errors

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -1,9 +1,19 @@
 return {
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = {
-        lsp_doc_border = true, -- add a border to hover docs and signature help
+  "folke/noice.nvim",
+  event = "VeryLazy",
+  -- TODO: Remove this once this issue is fixed:
+  -- https://github.com/yioneko/vtsls/issues/159
+  opts = {
+    presets = {
+      lsp_doc_border = true, -- add a border to hover docs and signature help
+    },
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "Request textDocument/inlayHint failed",
+        },
+        opts = { skip = true },
       },
     },
   },
