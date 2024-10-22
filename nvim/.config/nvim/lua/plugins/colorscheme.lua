86f5441e7bd43019a5a4602e925b131b9176a1b3
Axel Anderson <axel@ftml.net>
2024-10-22 12:17:18 -0400

    Use `nord` theme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,24 @@
+-- Nord
+return {
+  { "arcticicestudio/nord-vim" },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+    },
+  },
+}
+
+-- Tokyo Night
+-- return {
+--   "folke/tokyonight.nvim",
+--   opts = {
+--     transparent = true,
+--     styles = {
+--       sidebars = "transparent",
+--       floats = "transparent",
+--     },
+--   },
+-- }
+--
