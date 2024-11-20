99c47b5e6d37b7e9c9041c1d7f569b7e476c5cb8
Axel Anderson <axel@ftml.net>
2024-11-20 11:20:51 -0500

    Style nvim cli popup

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -16,5 +16,17 @@ return {
         opts = { skip = true },
       },
     },
+    views = {
+      cmdline_popup = {
+        border = {
+          style = "none",
+          padding = { 2, 3 },
+        },
+        filter_options = {},
+        win_options = {
+          winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder",
+        },
+      },
+    },
   },
 }
