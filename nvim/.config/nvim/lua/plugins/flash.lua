f24cb769d07afbfa9a4e716ba0e130a29f32274c
Axel Anderson <axel@ftml.net>
2025-01-27 12:59:10 -0500

    Use flash.nvim

--- nvim/.config/nvim/lua/plugins/flash.lua
+++ nvim/.config/nvim/lua/plugins/flash.lua
@@ -1,6 +1,27 @@
 return {
-  {
-    "folke/flash.nvim",
-    enabled = false,
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  ---@type Flash.Config
+  opts = {
+    modes = {
+      search = {
+        enabled = true,
+      },
+    },
+  },
+  keys = {
+    -- Disable the 's' key mapping
+    s = false,
+    -- Use 'f' for flash instead
+    f = true,
+    -- { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+    {
+      "S",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
   },
 }
