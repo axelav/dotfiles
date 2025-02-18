061fc910ecfc63a361ba6ba5a84e834df759b4ec
Axel Anderson <axel@ftml.net>
2025-02-18 08:59:20 -0500

    Change colorscheme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,27 +1,35 @@
 -- Tokyo Night
-return {
-  "folke/tokyonight.nvim",
-  opts = {
-    -- transparent = true,
-    -- styles = {
-    --   sidebars = "transparent",
-    --   floats = "transparent",
-    -- },
-    dim_inactive = true,
-  },
-}
--- Catppuccin
 -- return {
---   { "catppuccin/nvim" },
---
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       -- light
---       -- colorscheme = "catppuccin-latte",
---       -- dark
---       colorscheme = "catppuccin-frappe",
---       transparent = true,
---     },
+--   "folke/tokyonight.nvim",
+--   opts = {
+--     -- transparent = true,
+--     -- styles = {
+--     --   sidebars = "transparent",
+--     --   floats = "transparent",
+--     -- },
+--     dim_inactive = true,
 --   },
 -- }
+
+-- Catppuccin
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 0.15,
+      },
+      transparent_background = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "catppuccin-macchiato",
+    },
+  },
+}
