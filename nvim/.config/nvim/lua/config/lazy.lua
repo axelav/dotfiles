fa2dc9be4f6f4d890efaff2182f533216c6c5371
Axel Anderson <axel@ftml.net>
2025-07-30 22:54:51 -0400

    nvim: taller snacks terminal

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -247,6 +247,28 @@ require("lazy").setup({
       config = true,
     },
 
+    {
+      "folke/edgy.nvim",
+      ---@module 'edgy'
+      ---@param opts Edgy.Config
+      opts = function(_, opts)
+        for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
+          opts[pos] = opts[pos] or {}
+          table.insert(opts[pos], {
+            ft = "snacks_terminal",
+            size = { height = 0.9 },
+            title = "%{b:snacks_terminal.id}: %{b:term_title}",
+            filter = function(_buf, win)
+              return vim.w[win].snacks_win
+                  and vim.w[win].snacks_win.position == pos
+                  and vim.w[win].snacks_win.relative == "editor"
+                  and not vim.w[win].trouble_preview
+            end,
+          })
+        end
+      end,
+    },
+
     -- import/override with your plugins
     { import = "plugins" },
   },
