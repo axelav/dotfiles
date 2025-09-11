7b5fd54cde9dc4562c0e6a8d39b3428136af3e91
Axel Anderson <axel@ftml.net>
2025-09-10 20:54:10 -0400

    nvim: add claude code plugin

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -247,27 +247,15 @@ require("lazy").setup({
       config = true,
     },
 
-    -- {
-    --   "folke/edgy.nvim",
-    --   ---@module 'edgy'
-    --   ---@param opts Edgy.Config
-    --   opts = function(_, opts)
-    --     for _, pos in ipairs({ "top", "bottom", "left", "right" }) do
-    --       opts[pos] = opts[pos] or {}
-    --       table.insert(opts[pos], {
-    --         ft = "snacks_terminal",
-    --         size = { height = 0.9 },
-    --         title = "%{b:snacks_terminal.id}: %{b:term_title}",
-    --         filter = function(_buf, win)
-    --           return vim.w[win].snacks_win
-    --             and vim.w[win].snacks_win.position == pos
-    --             and vim.w[win].snacks_win.relative == "editor"
-    --             and not vim.w[win].trouble_preview
-    --         end,
-    --       })
-    --     end
-    --   end,
-    -- },
+    {
+      "greggh/claude-code.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim", -- Required for git operations
+      },
+      config = function()
+        require("claude-code").setup()
+      end,
+    },
 
     -- import/override with your plugins
     { import = "plugins" },
