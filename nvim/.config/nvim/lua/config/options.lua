1d87673cd8fe126ca503a31a0534d6d6e38eb068
Axel Anderson <axel@ftml.net>
2026-01-07 15:07:27 -0500

    nvim: set textwidth = 120

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Set text width for formatting commands (gw, gq, etc.)
+vim.opt.textwidth = 120
+
 -- Cursor settings to fix visibility issues
 vim.opt.guicursor =
   "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
